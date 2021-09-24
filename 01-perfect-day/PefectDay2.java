ab9d739c68cfbabcee1ea63b6ea99a642f9c4bb0
Mehreen Farihah <MehreenX@ac1mwk19njc9h.78b505.nycboe.org>
2021-09-24 12:26:44 -0400

    week 2 work

---
+++ 01-perfect-day/PefectDay2.java
@@ -0,0 +1,36 @@
+public class PerfectDay2 {
+  public static void main(string)[] args) {
+   System.out.println("My perfect day");
+   morning();
+
+  }// end main method
+
+  public static void morning() {
+    System.out.print("In the moring, wake up at 10:00am");
+    System.out.println("Make blueberry pancakes and have a coffee for breakfast");
+    System.out.Println("Go out for a 30min walk around the neighborhood");
+
+  }// end morning method
+
+  public static void afternoon() {
+    System.out.print("In the afternoon, make plans with my friends");
+    System.out.println("Drink bubble tea");
+    System.out.println("Get on a ferry ride and explore");
+
+  } // end afternoon method
+
+  public static void night() {
+    System.out.print("At night, order some Sushi for dinner");
+    System.out.println("Then clean my room and fix my bed");
+    System.out.printlin("Watch Netflix until I fall asleep");
+  }// end night medthod
+}// end class
+
+//Wake up at 10:00am in the morning
+//Make some pancakes with blueberries and have a coffee
+//Go out for a walk around the neighborhood for 30mins
+
+/*In the afternoon
+Make plans with my friends
+Drink bubble tea
+*/
