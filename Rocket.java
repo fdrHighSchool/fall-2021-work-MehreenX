4236e6e6531388b208f0aeb1ec458f19cc5efb62
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-09-29 00:10:33 -0400

    Add files via upload

---
+++ Rocket.java
@@ -0,0 +1,35 @@
+public class Rocket {
+  public static void main(String[] args) {
+  triangle();
+}// end main method
+
+  public static void triangle() {
+    System.out.println("    /\\");
+    System.out.println("   /  \\");
+    System.out.println("  /    \\");
+  }// end triangle method
+
+public static void box{
+  System.out.println("+------+");
+  System.out.println("|      |");
+  System.out.println("|      |");
+  System.out.println("+------+");
+}// end box method
+
+  public static void label{
+    System.out.println("|United|");
+    System.out.println("|States|");
+  }// end label method
+
+  public static void box{
+    System.out.println("+------+");
+    System.out.println("|      |");
+    System.out.println("|      |");
+    System.out.println("+------+");
+  }// end box method
+
+  public static void triangle() {
+    System.out.println("    /\\");
+    System.out.println("   /  \\");
+    System.out.println("  /    \\");
+  }// end triangle method
