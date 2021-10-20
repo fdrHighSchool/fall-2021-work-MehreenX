846b3b71fb071ed67994a3b5a5b20bf07127aaaa
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-10-19 21:46:21 -0400

    Add files via upload

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,19 +1,16 @@
 import java.util.Scanner; //importing scanner class
-public class RandomNumber {
+class Main {
   public static void main(String[] args) {
-    Scanner Num = new Scanner(System.in);
+      Scanner Num = new Scanner(System.in);
     //variables for the range
     int min = 1;
-    int max = 12;
-
+    int max = 100;
 
 
     //Generate random int value from 1 to 6
       System.out.println("Pick a random number from "+min+" to "+max+ ":");
       //user input
       int userAns = Num.nextInt();
-
-
 
       //set answer
       int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
@@ -23,20 +20,24 @@ public class RandomNumber {
         System.out.println("Yay, you did it T^T");
           }
       //when user guesses lower than the chosen number
-      if(userAns < random_int){
-        System.out.println("WRONGG, picked too low...");
+      else if (userAns < random_int){
+        System.out.println("WRONGG, picked too low...pick again");
+
+
       }
       //when user guesses higher than the chosen number
-      if(userAns > random_int){
+      else if(userAns > random_int){
         System.out.println("WRONGGG, picked too high...");
+
       }
-      while (userAns != random_int && userAns == random_int){
 
-            System.out.println("Try Again");
+     if (userAns != random_int) {
+            System.out.println( "Dumb Dum");
 
-
-      
+            System.out.println(
+                "The number was " + random_int +
+                " ,go away you can't play ahaha");
+     }
     }
-    }
 
-}
+} 
