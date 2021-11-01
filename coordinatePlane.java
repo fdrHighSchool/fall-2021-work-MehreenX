1c62dd3825dc07bf1c1125a3beb81b42cd96c637
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-11-01 08:03:47 -0400

    Add files via upload

--- coordinatePlane.java
+++ coordinatePlane.java
@@ -1,9 +1,50 @@
-import java.util,*;
+// imports
+import java.util.*;
 
-public class coordinatePlane {
-  public static void man (String)[] args) {
+public class CoordinatePlane {
+  public static void main(String[] args) {
+    // instantiate a scanner object
+    Scanner s = new Scanner(System.in);
 
+    System.out.print("please input a coordinate: ");
+    String point1 = s.nextLine();
 
+    int x1 = getX(point1);
+    System.out.println(x1);
 
+    System.out.println(x1 + 2);
+  }// end main method
+
+  /*
+   * Name: getX
+   * Purpose: extract the x-value from a point
+              convert the String to an int
+   * Input: String coordinate pair (input from main)
+   * Return: the x-value (int)
+  */
+  public static int getX(String point) {
+    int commaPos = point.indexOf(",");
+    // get the String version of x
+    String xVal = point.substring(1, commaPos);
+
+    // convert that String to an int
+    int x = Integer.parseInt(xVal);
+
+    // send the info back to main
+    return x;
   }
-} 
+
+  public static int getY(String point) {
+
+    return 0;
+  }
+
+  public static void crowsDistance(int x1, int y1, int x2, int y2) {
+
+  }// end crowsDistance method
+
+  public static void taxicabDistance(int x1, int y1, int x2, int y2) {
+
+  }// end taxicabDistance method
+
+}// end class
