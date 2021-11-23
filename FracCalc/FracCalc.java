cdf6d1108ae66ccb49de54a4d921f935aba339c8
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-11-22 23:00:23 -0500

    Tried adding the divisions and multiplication

--- FracCalc/FracCalc.java
+++ FracCalc/FracCalc.java
@@ -3,108 +3,104 @@ import java.util.*;
 
 public class FracCalc {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Fraction calculator: +, -, x, /");
     System.out.println("Type in your equation:");
 
     Scanner s = new Scanner(System.in);
 
-
-
     String choice = s.nextLine();
     System.out.println(produceAnswer(choice));
 
-  }//end main method
+  }// end main method
 
+  public static String produceAnswer(String input) {
 
- public static String produceAnswer(String input){
+    int space = input.indexOf(" ");
+    String Frac1 = input.substring(0, space);
+    String sign = input.substring(space + 1, space + 2);
+    String Frac2 = input.substring(space + 3);
 
-      int space = input.indexOf(" ");
-      String Frac1 = input.substring(0,space);
-      String sign = input.substring(space + 1, space + 2);
-      String Frac2 = input.substring(space + 3);
+    int num1 = getNum(Frac1);
+    int num2 = getNum(Frac2);
 
-      int num1 = getNum(Frac1);
-      int num2 = getNum(Frac2);
+    int den1 = getDen(Frac1);
+    int den2 = getDen(Frac2);
 
-      int den1 = getDen(Frac1);
-      int den2 = getDen(Frac2);
+    int Whole1 = getWhole(Frac1);
+    int Whole2 = getWhole(Frac2);
+    int Operation = getOperation(num1, den1, num2, dem2);
 
-      int Whole1 = getWhole(Frac1);
-      int Whole2 = getWhole(Frac2);
-      int Operation  = getOperation(num1, den1, num2, dem2);
+    return Frac2;
 
-      return Frac2;
+  }// end produceAnswer method
 
-
-  }//end produceAnswer method
-
-  public static int getNum(String Frac){
-      int slash = Frac.indexOf("/");
-      int Unders = Frac.indexOf("_");
+  public static int getNum(String Frac) {
+    int slash = Frac.indexOf("/");
+    int Unders = Frac.indexOf("_");
     // a mixed number
-    if (Frac.indexOf("_")!= -1){
-      return Integer.parseInt(Frac.substring(Unders+1,slash));
+    if (Frac.indexOf("_") != -1) {
+      return Integer.parseInt(Frac.substring(Unders + 1, slash));
     }
 
     // a regular frac (a/b)
-    else if (slash != -1){
-      return Integer.parseInt(Frac.substring(0,slash));
+    else if (slash != -1) {
+      return Integer.parseInt(Frac.substring(0, slash));
     }
 
     // not a mixed number (a)
     else {
-        return Integer.parseInt(Frac);
-      }
+      return Integer.parseInt(Frac);
+    }
 
   }
 
- public static int getDen(String Frac){
-   int slash = Frac.indexOf("/");
-   int Unders = Frac.indexOf("_");
-   if (slash == -1){ //no slash
-      return 1;
- }
- else{
-   return Integer.parseInt(Frac.substring(slash+1));// there's a slash, take dem
-   
-  }
- }
-
-
- public static int getWhole(String Frac){
+  public static int getDen(String Frac) {
     int slash = Frac.indexOf("/");
     int Unders = Frac.indexOf("_");
-    if (Unders != -1){
-      return Integer.parseInt(Frac.substring(0, Unders));
+    if (slash == -1) { // no slash
+      return 1;
+    } else {
+      return Integer.parseInt(Frac.substring(slash + 1));// there's a slash, take dem
+
     }
-    else{
-    return 0;
-  }
- }
-
-  public static int improperNum(int whole, int num, int den){
-       return (whole * den + num);
   }
 
-
-  public static String getOperation(int num1, int den1, int num2, int dem2){
-    int addNum = (num1*den2) + (num2*den1);
-    int commonDen = (den1 * den2);
-    if (operation.equals("+")){
-        return addNum ;
-  }else {
-        return commonDen ;
+  public static int getWhole(String Frac) {
+    int slash = Frac.indexOf("/");
+    int Unders = Frac.indexOf("_");
+    if (Unders != -1) {
+      return Integer.parseInt(Frac.substring(0, Unders));
+    } else {
+      return 0;
+    }
   }
-}
 
-  //public static int greatestCommonDivisor(int a, int b){
-    //return 0;
- // }//end greatestCommonDivisor method
+  public static int improperNum(int whole, int num, int den) {
+    return (whole * den + num);
+  }
 
+  public static String getOperation(int num1, int den1, int num2, int dem2) {
+    //int addNum = (num1 * den2) + (num2 * den1);
+    //int commonDen = (den1 * den2);
+    //if (operation.equals("+")) {
+   //   return addNum;
+   // } else {
+   //   return commonDen;
+    if (Operation.equals("*")){
+      return ((num1 * num2) / (den1 * den2));
+    }
+    else if (Operation.equals("/")){
+      return((num1 * dem2) / (num2 * den1));
+     }
+  }
 
-  //public static int leastCommonMultiple(int a, int b){
-    //return 0;
-  //}//end leastCommonMultiple
+  // public static int greatestCommonDivisor(int a, int b){
+  // return 0;
+  // }//end greatestCommonDivisor method
 
-}//end class
+  // public static int leastCommonMultiple(int a, int b){
+  // return 0;
+  // }//end leastCommonMultiple
+
+}// end class
