a0405f8385fade7684d0265cced3822bd5fe3b04
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-12-13 08:45:58 -0500

    Add files via upload

--- Mean/Mean100.java
+++ Mean/Mean100.java
@@ -1,26 +1,18 @@
 import java.util.*;
 
+
 public class Mean100{
   public static void main(String[] args) {
-    double arry[] = new double[100];
-    double i = 0;
+    int[] randomArray = new int[100];
 
-// 10 numbers that the mean = 5
-    arry[0] = 2;
-    arry[1] = 2;
-    arry[2] = 3;
-    arry[3] = 4;
-    arry[4] = 5;
-    arry[5] = 5;
-    arry[6] = 6;
-    arry[7] = 7;
-    arry[8] = 8;
-    arry[9] = 8;
+  for(int i = 0; i < 100; i++) {
+    int value = (int)(math.random()*100 + 1);
 
-//calculation of  mean
- double total = (2+2+3+4+5+5+6+7+8+8);
- double mean = total / arry.length;
- System.out.print("Mean = " + mean);
+    randomArray[i] = value
+  }
+  System.out.println(average(randomArray));
+
+
 
   }
 }
