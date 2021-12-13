26aa7723a79b4a093ac99031f3388e1b54f5ea0a
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-12-13 08:45:28 -0500

    working on random 100s

--- Mean/Mean.java
+++ Mean/Mean.java
@@ -4,6 +4,7 @@ public class Mean{
   public static void main(String[] args) {
        int arry[] = new int[10];
        int i = 0;
+       int[] randomArray = new int[100];
 
   // 10 numbers that the mean = 5
        arry[0] = 2;
@@ -16,13 +17,23 @@ public class Mean{
        arry[7] = 7;
        arry[8] = 8;
        arry[9] = 8;
+       System.out.print("2,2,3,4,5,5,6,7,8,8");
 
+  for(int i = 0; i < 100; i++) {
+      int value = (int)(math.random()*100 + 1);
+
+         randomArray[i] = value
+           System.out.println(average(randomArray));
+}
   //calculation of  mean
     int total = (2+2+3+4+5+5+6+7+8+8);
     int mean = total / arry.length;
-    System.out.print("Mean = " + mean);
+    System.out.println("Mean = " + mean);
 
-     }
+  public static Double average(int[] arr){
+    double total = 0.0;
   }
 
 
+     }
+  }
