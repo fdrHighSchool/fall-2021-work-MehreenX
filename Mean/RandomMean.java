036af7b19e9e9674a5e5a200602527a04f6f4211
MehreenX <91324906+MehreenX@users.noreply.github.com>
2021-12-14 08:46:57 -0500

    Add files via upload

--- Mean/RandomMean.java
+++ Mean/RandomMean.java
@@ -5,17 +5,44 @@ public class RandomMean{
      for(int i = 0; i < 100; i++) {
        double randomNumbers = (double)Math.round(Math.random()*100);
        arry[i] = randomNumbers;
+
+       System.out.println(Arrays.toString(randomNumbers));
+
    }
 }
    //Average
-   public static double avg(int[] arry) {
-  double total = 0.0;
-for(int i=0; i<arry.length; i++) {
-total += arry[i];
+  public static double avg(int[] arry) {
+   double total = 0.0;
+   for(int i=0; i<arry.length; i++) {
+   total += arry[i];
 }
-double average = total / arry.length;
-System.out.println(Arrays.toString(arry));
-return average;
-}
+   double average = total / arry.length;
 
+   return average;
 }
+  public static int mode(int[] arr){
+    int max = findMax(arr);
+
+    int[] freq = new int [max + 1];
+
+    for (int i = 0; i < arry.length; i++){
+      freq[arr[i]]++;
+
+    }
+    Syetem.out.println(Arrays.toString(freq));
+    return 0;
+
+  }//end mode
+  public statuc int findMax(int[] arr){
+    int max = -99999;
+
+    for(int i = 0; i < arry.length; i++){
+      if(arr[i] > max{
+        max = arr [i];
+      }// end if
+    }// end loop
+
+    return max;
+  }//end max
+
+}// class
